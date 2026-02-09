dbec362a1397246b5205ca0ec8f361f8d6c4cdd6
Alex St-Onge <stonge.alex@gmail.com>
2026-02-09 06:28:16 -0800

    Fix mac buid

--- cmake/llvm-toolchain.cmake
+++ cmake/llvm-toolchain.cmake
@@ -1,12 +1,10 @@
 include_guard(GLOBAL)
 
-if(APPLE)
-    set(CMAKE_CXX_COMPILER "/opt/homebrew/opt/llvm@20/bin/clang++")
-    set(CMAKE_C_COMPILER "/opt/homebrew/opt/llvm@20/bin/clang")
-else()
-    set(CMAKE_CXX_COMPILER "clang++")
-    set(CMAKE_C_COMPILER "clang")
-endif()
+# if(APPLE) set(CMAKE_CXX_COMPILER "/opt/homebrew/opt/llvm@20/bin/clang++") set(CMAKE_C_COMPILER
+# "/opt/homebrew/opt/llvm@20/bin/clang") else()
+set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_C_COMPILER "clang")
+# endif()
 
 if(WIN32)
     set(CMAKE_SYSTEM_LIBRARY_PATH "$ENV{ProgramFiles}/LLVM/lib")
